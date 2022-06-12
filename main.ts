ff7be0539fe974ba00419673fc6eae0ecacd92d1
Barobi <87016737+Barobi@users.noreply.github.com>
2022-06-12 13:45:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,23 +14,23 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             basic.showString("A")
         } else if (control.eventValue() == 3) {
             basic.showString("B")
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.motorStopAll()
         } else if (control.eventValue() == 5) {
             basic.showString("C")
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
-        } else if (control.eventValue() == 7) {
-            basic.showString("D")
             motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
             motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+        } else if (control.eventValue() == 7) {
+            basic.showString("D")
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
         } else if (control.eventValue() == 9) {
             basic.showString("1")
             motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
             motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
         } else if (control.eventValue() == 11) {
             basic.showString("2")
-            motor.motorStopAll()
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
         } else if (control.eventValue() == 13) {
             basic.showString("3")
             motor.motorStop(motor.Motors.M2)
