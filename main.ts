501f59fd3e42cbadb6234caf0e4c71c62875233e
Barobi <87016737+Barobi@users.noreply.github.com>
2022-06-12 14:07:17 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,58 @@
+function c () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+}
+function _2 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+}
+function b () {
+    motor.motorStopAll()
+}
+function a () {
+    motor.motorStopAll()
+}
+function _3 () {
+    motor.motorStop(motor.Motors.M2)
+    basic.pause(200)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+}
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (lastValue != control.eventValue()) {
         lastValue = control.eventValue()
-        led.stopAnimation()
-        // A
-        // B
-        // C
-        // D
-        // 1
-        // 2
-        // 3
-        // 4
         if (control.eventValue() == 1) {
-            basic.showString("A")
+            a()
         } else if (control.eventValue() == 3) {
-            basic.showString("B")
-            motor.motorStopAll()
+            b()
         } else if (control.eventValue() == 5) {
-            basic.showString("C")
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            c()
         } else if (control.eventValue() == 7) {
-            basic.showString("D")
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+            d()
         } else if (control.eventValue() == 9) {
-            basic.showString("1")
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            _1()
         } else if (control.eventValue() == 11) {
-            basic.showString("2")
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            _2()
         } else if (control.eventValue() == 13) {
-            basic.showString("3")
-            motor.motorStop(motor.Motors.M2)
-            basic.pause(200)
-            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            _3()
         } else if (control.eventValue() == 15) {
-            basic.showString("4")
-            motor.motorStop(motor.Motors.M1)
-            basic.pause(200)
-            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
-        } else {
-            basic.clearScreen()
+            _4()
         }
     }
 })
+function d () {
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+}
+function _4 () {
+    motor.motorStop(motor.Motors.M1)
+    basic.pause(200)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+}
+function _1 () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+}
 let speed = 0
 let lastValue = 0
 lastValue = 0
-speed = 80
-bluetooth.startLEDService()
-basic.showString("GAMEPAD DEMO")
+speed = 96
