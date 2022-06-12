a6cb0767b89bcadd8c0717c1068d5099da32bb9b
Barobi <87016737+Barobi@users.noreply.github.com>
2022-06-12 09:51:54 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,10 +10,13 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
         } else if (control.eventValue() == 5) {
             basic.showString("C")
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 255)
         } else if (control.eventValue() == 7) {
             basic.showString("D")
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
         } else if (control.eventValue() == 9) {
             basic.showString("1")
+            motor.motorStopAll()
         } else if (control.eventValue() == 11) {
             basic.showString("2")
         } else if (control.eventValue() == 13) {
