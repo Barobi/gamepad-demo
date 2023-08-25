86eb6aa321902caf16a135f479c8a32a09500351
Barobi <87016737+Barobi@users.noreply.github.com>
2023-08-25 13:10:27 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@ function _4_up () {
     motor.motorStop(motor.Motors.M2)
 }
 function c_down () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, motorSpeed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, motorSpeed)
 }
 function c_up () {
     motor.motorStop(motor.Motors.M1)
 }
 function _4_down () {
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, motorSpeed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, motorSpeed)
 }
 function b_down () {
     motorSpeed += -16
@@ -17,14 +17,14 @@ function b_up () {
 	
 }
 function _3_down () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, motorSpeed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, motorSpeed)
 }
 function _1_down () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, motorSpeed)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, motorSpeed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, motorSpeed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, motorSpeed)
 }
 function d_down () {
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, motorSpeed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, motorSpeed)
 }
 function a_down () {
     motorSpeed += 16
@@ -41,8 +41,8 @@ function d_up () {
     motor.motorStop(motor.Motors.M2)
 }
 function _2_down () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, motorSpeed)
-    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, motorSpeed)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, motorSpeed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, motorSpeed)
 }
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (prevButtonEvent != control.eventValue()) {
